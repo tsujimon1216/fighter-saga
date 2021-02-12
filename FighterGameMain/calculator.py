58ffb6b18c32765280216b8be7f6ff4b12feedf6
tsujimon1216 <ryoji1216@outlook.jp>
2021-02-13 01:43:50 +0900

    calculaterの中身を実装

--- FighterGameMain/calculator.py
+++ FighterGameMain/calculator.py
@@ -1,8 +1,20 @@
+import random
+
+
 def make_rand_num():
-    pass
+    return random.randint(1,9)
+
 
 def questions():
-    pass
+    questions_answer = make_rand_num()*make_rand_num()
+    return questions_answer
+
 
 def check_the_answer():
-    pass
+    input_answer = input('input_answer : ')
+    if questions() == int(input_answer):
+        return True
+    else:
+        return False
+
+
