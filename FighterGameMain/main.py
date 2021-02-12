b49f59f82a802a828064871619983f286f20352f
tsujimon1216 <ryoji1216@outlook.jp>
2021-02-13 01:01:18 +0900

    pycharmからcommitのテスト

--- FighterGameMain/main.py
+++ FighterGameMain/main.py
@@ -8,6 +8,3 @@ from monster import *
 monster1 = Monster(1, "monster1", 100, 30, 10)
 monster2 = Monster(2, "monster2", 200, 60, 20)
 
-while monster1.life_point > 0 and monster2.life_point > 0:
-    monster1.attack_to(monster2)
-    monster2.attack_to(monster1)
